d4dfd0d449185eddf7d6ac7db0bfff79ef06a691
alireza-sharifpour <ascode94@gmail.com>
2024-02-03 15:51:58 +0330

    update logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import Image from 'next/image'
 
 const Header = () => {
   return (
@@ -13,7 +14,12 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
-              <Logo />
+              <Image
+                src="/static/images/chimpa-logo.png"
+                width={80}
+                height={100}
+                alt="chimpavision logo"
+              />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
