b750d275615ed645e7c2db132f795da57d27eb34
alireza-sharifpour <ascode94@gmail.com>
2024-02-06 10:42:45 +0330

    add google meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,6 +72,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+      <meta name="google-site-verification" content="vCgAGyFCLzyKel1WpXZF9hIfuAc3CdDLD7_ncnqZVoE" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
