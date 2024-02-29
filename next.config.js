8d60e5588b5070734f5c2e5d6bb0368114b2e3e9
alireza-sharifpour <ascode94@gmail.com>
2024-02-29 17:48:03 +0330

    add unsafe-eval for google

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
